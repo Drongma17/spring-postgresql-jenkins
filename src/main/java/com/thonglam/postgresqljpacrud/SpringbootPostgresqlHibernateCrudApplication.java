073c38126a00e0b7031c2e52a48935d8032ec5e7
Drongma17 <30928492+Drongma17@users.noreply.github.com>
2021-07-27 17:59:24 -0700

    third commit

--- src/main/java/com/thonglam/postgresqljpacrud/SpringbootPostgresqlHibernateCrudApplication.java
+++ src/main/java/com/thonglam/postgresqljpacrud/SpringbootPostgresqlHibernateCrudApplication.java
@@ -2,12 +2,24 @@ package com.thonglam.postgresqljpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringbootPostgresqlHibernateCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootPostgresqlHibernateCrudApplication.class, args);
+
+
+
+		RestTemplate restTemplate=new RestTemplate();
+		List response = restTemplate.getForObject("http://jsonplaceholder.typicode.com/posts", List.class);
+		response.forEach(post->{
+			System.out.println(post);
+			System.out.println("");
+		});
 	}
 
 }
---
+++ src/main/java/com/thonglam/postgresqljpacrud/controller/APIController.java
@@ -0,0 +1,11 @@
+package com.thonglam.postgresqljpacrud.controller;
+
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class APIController {
+
+
+
+}
